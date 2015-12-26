e8b8cd5adc7a2d9ed1b8f0b64be3e866430a1ba0
Paul Bogard <paulbogard@gmail.com>
2015-12-26 00:19:46 -0500

    Add table of airlines to Index Airlines

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -3,7 +3,85 @@ class AirlinesController < ApplicationController
   add_breadcrumb 'Home', 'root_path'
   
   def index
-    @airlines_with_no_flights = Airline.all.order(:airline_name)
+    @logo_used = true
+    add_breadcrumb 'Airlines', 'airlines_path'
+    if logged_in?
+      @flight_airlines = Flight.where("airline_id IS NOT NULL").group("airline_id").count
+      @flight_operators = Flight.where("operator_id IS NOT NULL").group("operator_id").count
+    else # Filter out hidden trips for visitors
+      @flight_airlines = Flight.visitor.where("airline IS NOT NULL").group("airline").count
+      @flight_operators = Flight.visitor.where("operator IS NOT NULL").group("operator").count
+    end
+    used_airline_ids = (@flight_airlines.keys + @flight_operators.keys).uniq
+    @airlines_with_no_flights = Airline.where("id NOT IN (?)", used_airline_ids).order(:airline_name) #UPDATE
+    
+    
+    
+    @title = "Airlines"
+    @meta_description = "A list of the airlines on which Paul Bogard has flown, and how often he's flown on each."
+    
+    @airlines_array = Array.new
+    @operators_array = Array.new
+    
+    if (@flight_airlines.any? || @flight_operators.any?)
+      
+      @airline_details = Airline.select("id, iata_airline_code, airline_name").find(used_airline_ids)
+      @airline_names = Hash.new
+      @airline_iata_codes = Hash.new
+      @airline_details.each do |airline|
+        @airline_names[airline.id] = airline.airline_name
+        @airline_iata_codes[airline.id] = airline.iata_airline_code 
+      end
+      
+      # Set values for sort:
+      case params[:sort_category]
+      when "airline"
+        @sort_cat = :airline
+      when "flights"
+        @sort_cat = :flights
+      else
+        @sort_cat = :flights
+      end
+    
+      case params[:sort_direction]
+      when "asc"
+        @sort_dir = :asc
+      when "desc"
+        @sort_dir = :desc
+      else
+        @sort_dir = :desc
+      end
+    
+      sort_mult = (@sort_dir == :asc ? 1 : -1)
+      
+      # Prepare airline list:
+      @flight_airlines.each do |airline, count| 
+        @airlines_array.push({:airline => airline, :count => count})
+      end
+    
+      # Prepare operator list:
+      @flight_operators.each do |operator, count|
+        @operators_array.push({:operator => operator, :count => count})
+      end
+      
+      # Find maxima for graph scaling:
+      @airlines_maximum = @airlines_array.any? ? @airlines_array.max_by{|i| i[:count]}[:count] : 0
+      @operators_maximum = @operators_array.any? ? @operators_array.max_by{|i| i[:count]}[:count] : 0
+    
+      # Sort airline and operator tables:
+      case @sort_cat
+      when :airline
+        @airlines_array = @airlines_array.sort_by { |airline| airline[:airline] }
+        @operators_array = @operators_array.sort_by { |operator| operator[:operator] }
+        @airlines_array.reverse! if @sort_dir == :desc
+        @operators_array.reverse! if @sort_dir == :desc
+      when :flights
+        @airlines_array = @airlines_array.sort_by { |airline| [sort_mult*airline[:count], airline[:airline]] }
+        @operators_array = @operators_array.sort_by { |operator| [sort_mult*operator[:count], operator[:operator]] }
+      end
+    end
+    
+    
   end
   
   def show
@@ -61,16 +139,17 @@ class AirlinesController < ApplicationController
   end
   
   def destroy
+    if (Airline.exists?(params[:id]))
+      @airline = Airline.find(params[:id])
+    else
+      @airline = Airline.where(:iata_airport_code => params[:id]).first
+    end
     @flights = @airline.all_flights(true)
     if @flights.any?
       flash[:error] = "This airline still has flights and could not be deleted. Please delete all of this airline's flights first."
       redirect_to airline_path(params[:id])
     else
-      if (Airline.exists?(params[:id]))
-        Airline.find(params[:id]).destroy
-      else
-        Airline.where(:iata_airport_code => params[:id]).first.destroy
-      end
+      @airline.destroy
       flash[:success] = "Airline deleted."
       redirect_to airlines_path
     end
