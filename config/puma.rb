725193e6275f606749256cab55436ba5460e69a1
Paul Bogard <paulbogard@gmail.com>
2016-01-19 18:03:34 -0500

    Convert production webserver to Puma

---
+++ config/puma.rb
@@ -0,0 +1,14 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
+  ActiveRecord::Base.establish_connection
+end
