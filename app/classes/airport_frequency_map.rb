bfde6729604a288a4fac7f8c0f3cf568432f9302
Paul Bogard <paulbogard@gmail.com>
2016-08-28 22:10:50 -0400

    Refactor AirportFrequencyMap class

--- app/classes/airport_frequency_map.rb
+++ app/classes/airport_frequency_map.rb
@@ -7,17 +7,8 @@ class AirportFrequencyMap < Map
   # +flights+:: A collection of Flights.
   # +region+:: The region to show. World map will be shown if region is left blank.
   def initialize(flights, region: :world)
-        
-    airport_frequencies = Airport.frequency_hash(flights)
-    @airport_codes = Airport.region_iata_codes(flights, region)
-    
-    @airport_array = Array.new
-    @airport_codes.each do |airport, code|
-      @airport_array.push(iata_code: code, frequency: airport_frequencies[airport])
-    end
-    @airport_array = @airport_array.sort_by { |airport| [-airport[:frequency], airport[:iata_code]] }
-    
-    
+    @airport_codes = Airport.region_iata_codes(flights, region)    
+    @airport_frequencies = Airport.frequency_hash(flights)  
   end
   
   private
@@ -39,16 +30,22 @@ class AirportFrequencyMap < Map
       frequency_max = 1.0
       frequency_scaled = 0
       
-      query = Array.new
+      query = Array.new      
+      region_frequencies = Array.new
       
-      @airport_array.each do |airport|
-        if airport == @airport_array.first
+      @airport_codes.each do |airport, code|
+        region_frequencies.push(iata_code: code, frequency: @airport_frequencies[airport])
+      end
+      region_frequencies.sort_by! { |airport| [-airport[:frequency], airport[:iata_code]] }
+      
+      region_frequencies.each do |airport|
+        if airport == region_frequencies.first
           # This is the first circle, so define its color:
           query.push("m:p:ring#{max_gcmap_ring}:black")
           query.push(airport[:iata_code])
           frequency_max = airport[:frequency].to_f
         elsif airport[:frequency] == previous_airport_value
-          # Value is the same as previous, so no need to define circle size:
+          # Value is the same as previous, so no need to define ring size:
           query.push(airport[:iata_code])
         else
           frequency_scaled = Math.sqrt((airport[:frequency].to_f / frequency_max)*(max_gcmap_ring**2)).ceil.to_i # Scale frequency range from 1..max_gcmap_ring
