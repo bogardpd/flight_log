2aa1e3bebde7531bfa52b14f1a5599dd14f2079d
Paul Bogard <paulbogard@gmail.com>
2016-09-01 17:39:51 -0400

    Use environment variable for secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Portfolio::Application.config.secret_token = 'bc12145de2a0b4268db92192b0aa7d941789a4970821df62a639b1b58dd2e93596b51f7621e552fa28065ed320bb738366880a276c5c4c22b9f9d87cd008b5b1'
+Portfolio::Application.config.secret_token = ENV["SECRET_KEY_BASE"]
