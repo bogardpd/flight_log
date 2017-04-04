2bf48eb21adaa9883e05111303388878689f530f
Paul Bogard <paulbogard@gmail.com>
2017-04-03 20:14:11 -0400

    Add flight_id column to PKPass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323220630) do
+ActiveRecord::Schema.define(version: 20170404001311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170323220630) do
     t.datetime "received"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "flight_id"
     t.index ["serial_number"], name: "index_pk_passes_on_serial_number", using: :btree
   end
 
