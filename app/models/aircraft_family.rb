a4e55ba85aecbf47e0852797e3ac895ca270c7e0
Paul Bogard <paulbogard@gmail.com>
2017-04-17 19:33:55 -0400

    Update Index Aircraft page to roll up child types into parent family

--- app/models/aircraft_family.rb
+++ app/models/aircraft_family.rb
@@ -3,6 +3,7 @@ class AircraftFamily < ApplicationRecord
   has_many :children, class_name: "AircraftFamily", foreign_key: "parent_id"
   has_many :flights
   
+  scope :families, -> { where(parent_id: nil) }
   
   def self.categories_list
     categories = Hash.new
@@ -26,14 +27,15 @@ class AircraftFamily < ApplicationRecord
     return self.manufacturer + " " + self.family_name
   end
   
-  # Returns an array of aircraft families, with a hash for each family
-  # containing the aircraft manufacturer, name, IATA code, and number
-  # of flights on that aircraft, sorted by number of flights descending.
+  # Returns an array of aircraft families (only those without parents), with a
+  # hash for each family containing the aircraft manufacturer, name, IATA code,
+  # and number of flights on that aircraft, sorted by number of flights
+  # descending. Child types are included with the parent family count.
   def self.flight_count(logged_in=false)
     flights = logged_in ? Flight.all : Flight.visitor
-    flights.joins(:aircraft_family).group(:aircraft_family_id, :manufacturer, :family_name, :iata_aircraft_code).count
-      .map{|k,v| {id: k[0], manufacturer: k[1], family_name: k[2], iata_aircraft_code: k[3], flight_count: v}}
-      .sort_by{|a| [-a[:flight_count], a[:manufacturer], a[:family_name]]}
+    family_count = Hash.new(0)
+    flights.joins(:aircraft_family).group(:aircraft_family_id, :parent_id).count.map{|k,v| family_count[k[1]||k[0]] += v}
+    self.families.map{|f| {manufacturer: f.manufacturer, family_name: f.family_name, iata_aircraft_code: f.iata_aircraft_code, flight_count: family_count[f.id] || 0}}.sort_by{|a| [-a[:flight_count], a[:manufacturer], a[:family_name]]}
   end
 
 end
