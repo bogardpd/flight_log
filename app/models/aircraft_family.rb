762efe9c451893e85be11c26a8a9714b40275ac8
Paul Bogard <paulbogard@gmail.com>
2017-04-17 20:23:35 -0400

    Update AircraftFamily.flight_count to use map/reduce

--- app/models/aircraft_family.rb
+++ app/models/aircraft_family.rb
@@ -33,8 +33,10 @@ class AircraftFamily < ApplicationRecord
   # descending. Child types are included with the parent family count.
   def self.flight_count(logged_in=false)
     flights = logged_in ? Flight.all : Flight.visitor
-    family_count = Hash.new(0)
-    flights.joins(:aircraft_family).group(:aircraft_family_id, :parent_id).count.map{|k,v| family_count[k[1]||k[0]] += v}
+    family_count = flights.joins(:aircraft_family).group(:aircraft_family_id, :parent_id).count
+      .map{|k,v| {(k[1]||k[0]) => v}} # Create array of hashes with k as parent id or family id and v as count
+      .reduce{|a,b| a.merge(b){|k,old_v,new_v| old_v + new_v}} # Group and sum family counts
+      
     self.families.map{|f| {manufacturer: f.manufacturer, family_name: f.family_name, iata_aircraft_code: f.iata_aircraft_code, flight_count: family_count[f.id] || 0}}.sort_by{|a| [-a[:flight_count], a[:manufacturer], a[:family_name]]}
   end
 
