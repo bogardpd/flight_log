8b60220cf975fcd7b880bae969c2ba042eb9fc45
Paul Bogard <paulbogard@gmail.com>
2017-06-25 19:51:45 -0400

    Write integration tests for Show Flight, Show Year, and Show Date Range

--- test/controllers/flights_controller_test.rb
+++ test/controllers/flights_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class FlightsControllerTest < ActionDispatch::IntegrationTest
   
@@ -7,11 +7,21 @@ class FlightsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # def test_show_flight_success
-  #   flight = flights(:flight1)
-  #   get flight_path(flight)
-  #   assert_response :success
-  # end
+  def test_show_flight_success
+    flight = flights(:flightORDDFW)
+    get flight_path(flight)
+    assert_response :success
+  end
+  
+  def test_show_year_success
+    get show_year_path(year: 2015)
+    assert_response :success
+  end
+  
+  def test_show_date_range_success
+    get show_date_range_path(start_date: "2014-07-01", end_date: "2015-06-30")
+    assert_response :success
+  end
 
   def test_index_tails_success
     get tails_path
