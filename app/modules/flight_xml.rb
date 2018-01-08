568bb946356b16a362cbc1df01ce3fd3b7fdb38e
Paul Bogard <paulbogard@gmail.com>
2018-01-08 09:30:05 -0500

    Write flight_info(fa_flight_id) method for FlightXML module

--- app/modules/flight_xml.rb
+++ app/modules/flight_xml.rb
@@ -42,7 +42,32 @@ module FlightXML
     rescue
       return nil
     end
-    
+  end
+  
+  # Accepts a FlightXML fa_flight_id, and returns information about the flight.
+  def self.flight_info(fa_flight_id)
+    begin
+        
+      flight_info_ex = client.call(:flight_info_ex, message: {
+        ident: fa_flight_id,
+        how_many: 1,
+        offset: 0
+        }).to_hash[:flight_info_ex_results][:flight_info_ex_result][:flights]
+      
+      airline_flight_info = client.call(:airline_flight_info, message: {
+        fa_flight_i_d: flight_id
+        }).to_hash[:airline_flight_info_results][:airline_flight_info_result]
+      
+      output = {
+        aircraft_type: flight_info_ex[:aircrafttype],
+        tail_number: airline_flight_info[:tailnumber],
+        operator: flight_id[0,3],
+        codeshares: airline_flight_info[:codeshares]
+      }
+      return output
+    rescue
+      return nil
+    end
   end
   
 end
