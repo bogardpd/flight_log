e14dd77187445798a28351fe17294c88043fdc0f
Paul Bogard <paulbogard@gmail.com>
2018-02-13 04:44:52 +0800

    Add Oceania to regions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -116,6 +116,7 @@ module ApplicationHelper
     regions["World"]       = %w()
     regions["USA (CONUS)"] = %w(K)
     regions["Europe"]      = %w(B E L)
+    regions["Oceania"]     = %w(N PH Y)
     
     used_airports = map.used_airports.sort
     tabs = Array.new
