5ce3eaf3af103c67f1417d859a98698e7ad99c47
Paul Bogard <paulbogard@gmail.com>
2018-10-14 15:19:52 -0400

    Move admin breadcrumbs to individual admin controller methods

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,19 +1,19 @@
 class AdminController < ApplicationController
   before_action :logged_in_user
-  
-  #add_breadcrumb "Admin", admin_path
-  
-  def admin
     
+  def admin
+    add_breadcrumb "Admin", admin_path
   end
   
   def annual_flight_summary
+    add_breadcrumb "Admin", admin_path
     add_breadcrumb "Annual Flight Summary", annual_flight_summary_path
     @title = "Annual Flight Summary"
     @flight_summary = Flight.by_year
   end
   
   def boarding_pass_validator
+    add_breadcrumb "Admin", admin_path
     add_breadcrumb "Boarding Pass Validator", boarding_pass_validator_path
     @title = "Boarding Pass Validator"
     @pass_flights = Flight.select(:id, :boarding_pass_data).where("boarding_pass_data IS NOT NULL").order(:departure_utc)
