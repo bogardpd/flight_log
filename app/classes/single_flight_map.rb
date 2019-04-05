dc505f171c703a21c6c7889b7d307f224fe8474c
Paul Bogard <paulbogard@gmail.com>
2019-04-05 13:13:02 -0400

    Rewrite SingleFlightMap to use updated Map class

--- app/classes/single_flight_map.rb
+++ app/classes/single_flight_map.rb
@@ -4,7 +4,7 @@ class SingleFlightMap < Map
   # Params:
   # +flight+:: An instance of the Flight model
   def initialize(flight)
-    #@codes = [flight.origin_airport.iata_code, flight.destination_airport.iata_code]
+    @codes = [flight.origin_airport.iata_code, flight.destination_airport.iata_code]
     #@route = [@codes.join("-")]
     @flight = flight
   end
@@ -15,19 +15,14 @@ class SingleFlightMap < Map
     def routes_normal
       return [[@flight.origin_airport_id, @flight.destination_airport_id].sort]
     end
-    
-    # Old methods:
 
-    # def airport_options
-    #   return "*"
-    # end
-    
-    # def alt_tag
-    #   "Map of flight route between #{@codes[0]} and #{@codes[1]}"
-    # end
-    
-    # def routes_inside_region
-    #   return @route
-    # end
+    def map_description
+      "Map of flight route between #{@codes[0]} and #{@codes[1]}"
+    end
+
+    # Returns a string of Great Circle Mapper airport options.
+    def gcmap_airport_options
+      return "*"
+    end
   
 end
