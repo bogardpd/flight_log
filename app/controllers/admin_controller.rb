cd99de19aa0b73ac655baac9786793fe56b2e591
Paul Bogard <paulbogard@gmail.com>
2019-05-09 19:28:04 -0400

    Document AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,15 @@
+# Controls administrative pages. Users must be logged in to view any of these pages.
+
 class AdminController < ApplicationController
   before_action :logged_in_user
-    
+  
+  # Shows the administrative main menu.
   def admin
     add_breadcrumb "Admin", admin_path
   end
   
+  # Shows a table of the total count of business, mixed, and personal {Flight}s
+  # for each calendar year.
   def annual_flight_summary
     add_breadcrumb "Admin", admin_path
     add_breadcrumb "Annual Flight Summary", annual_flight_summary_path
@@ -12,6 +17,8 @@ class AdminController < ApplicationController
     @flight_summary = Flight.by_year
   end
   
+  # Shows the boarding pass data of all {Flight}s with invalid boarding pass
+  # data.
   def boarding_pass_validator
     add_breadcrumb "Admin", admin_path
     add_breadcrumb "Boarding Pass Validator", boarding_pass_validator_path
