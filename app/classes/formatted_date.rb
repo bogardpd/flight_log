c9cbc6af1c6a5c4ee5b472a7f0853c275bc448e9
Paul Bogard <paulbogard@gmail.com>
2019-05-09 19:19:15 -0400

    Document FormattedDate class

--- app/classes/formatted_date.rb
+++ app/classes/formatted_date.rb
@@ -1,5 +1,9 @@
+# Defines a Date with specific formatting.
 class FormattedDate < Date
   
+  # Defines a standard date format string to use throughout the application.
+  #
+  # @return [String] a formatted date
   def standard_date
     return strftime("%-d %b %Y")
   end
