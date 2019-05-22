68cdfe57feda0855b95022a8454887bd563c183c
Paul Bogard <paulbogard@gmail.com>
2019-05-21 20:28:00 -0500

    Document PKPassesController

--- app/controllers/pk_passes_controller.rb
+++ app/controllers/pk_passes_controller.rb
@@ -1,6 +1,21 @@
+# Manages the {PKPass} {#destroy} action.
+#
+# No other methods are provided for {PKPass PKPasses} are provided here, since
+# {PKPass PKPasses} are only created by parsing raw {BoardingPass} barcode
+# data, are only viewed in the context of creating a
+# {FlightsController#new_flight_menu new} {Flight} or through the
+# {BoardingPass} {FlightsController#show_boarding_pass parser}, and are
+# uneditable.
+#
+# @see BoardingPass
+# @see FlightsController#new_flight_menu
+# @see FlightsController#show_boarding_pass
 class PkPassesController < ApplicationController
   before_action :logged_in_user
   
+  # Deletes an existing {PKPass}.
+  #
+  # @return [nil]
   def destroy
     PKPass.find(params[:id]).destroy
     flash[:success] = "Pass destroyed."
@@ -8,7 +23,5 @@ class PkPassesController < ApplicationController
   end
   
   private
-  
-    
   
 end
