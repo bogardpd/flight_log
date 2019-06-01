fb955ac029d2a6d5459e0352181dd77819e43766
Paul Bogard <paulbogard@gmail.com>
2019-05-31 22:54:37 -0400

    Document PKPass

--- app/models/pk_pass.rb
+++ app/models/pk_pass.rb
@@ -1,13 +1,28 @@
-###############################################################################
-# Defines a boarding pass using Apple's PassKit Package (PKPass) format.      #
-###############################################################################
-
+# Defines a boarding pass using Apple's PassKit Package (PKPass) format. This
+# is used to store Apple Wallet (iOS) boarding passes {BoardingPassEmail
+# received} at a specified email address.
+#
+# PKPass boarding passes are a compressed folder containing a JSON file, which
+# itself includes {BoardingPass} barcode data, along with some other metadata
+# that can be used to prepopulate {Flight} form fields.
+#
+# Since all possible flight data is extracted from the PKPass upon creating a
+# new {Flight}, the PKPass does not maintain a direct database relationship
+# with the {Flight} model. Instead, once a new {Flight} is created, its PKPass
+# is destroyed.
+# 
+# @see BoardingPassEmail
+# @see https://developer.apple.com/documentation/passkit/wallet Wallet | Apple Developer Documentation
 class PKPass < ApplicationRecord
   after_initialize :set_values
   
   validates :pass_json, :presence => true
   
-  # Returns the pass's barcode string
+  # Returns the pass's BCBP barcode data string
+  #
+  # @return [String] barcode text
+  # @see https://www.iata.org/whatwedo/stb/Documents/BCBP-Implementation-Guide-5th-Edition-June-2016.pdf
+  #   IATA Bar Coded Boarding Pass (BCBP) Implementation Guide
   def barcode
     if @pass.dig("barcodes")
       return @pass.dig("barcodes", 0, "message")
@@ -16,12 +31,16 @@ class PKPass < ApplicationRecord
     end
   end
   
-  # Returns a BoardingPass based on the PKPass's barcode field
+  # Returns a {BoardingPass} based on the PKPass's barcode field.
+  #
+  # @return [BoardingPass] a new {BoardingPass} created from barcode data
   def bcbp
     return BoardingPass.new(barcode)
   end
   
-  # Returns a hash of form default values for this pass
+  # Returns a hash of form field values extracted from this PKPass.
+  #
+  # @return [Hash] a hash with field names as keys, and field values as values
   def form_values
     fields = Hash.new
     
@@ -50,8 +69,12 @@ class PKPass < ApplicationRecord
     return fields
   end
   
-  # Returns an array of hashes of summary details for all boarding passes
-  # that are not yet associated with a flight.
+  # Returns an array of hashes of summary details for all PKPasses that are not
+  # yet associated with a {Flight}. Used on {FlightsController#new_flight_menu}
+  # to show a list of boarding passes that the user can choose to create a
+  # {Flight} from.
+  #
+  # @return [Array<Hash>] all boarding passes not associated with a {Flight}
   def self.pass_summary_list
     PKPass.where(flight_id: nil).map{|pass|
       fields = BoardingPass.new(pass.barcode, interpretations: false).summary_fields
@@ -67,10 +90,13 @@ class PKPass < ApplicationRecord
   end
   
   protected
-  
-    def set_values
-      @pass = JSON.parse(self.pass_json)
-      self.assign_attributes({:serial_number => [@pass.dig("passTypeIdentifier"),@pass.dig("serialNumber")].join(",")})
-    end
+
+  # Initializes certain values for the boarding pass.
+  #
+  # @return [Hash]
+  def set_values
+    @pass = JSON.parse(self.pass_json)
+    self.assign_attributes({:serial_number => [@pass.dig("passTypeIdentifier"),@pass.dig("serialNumber")].join(",")})
+  end
 
 end
