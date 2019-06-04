89a2ca8f378679070657db00cacc163bfa1fdea5
Paul Bogard <paulbogard@gmail.com>
2019-06-03 20:51:25 -0400

    Document Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,37 @@
+# Defines a model for trips.
+# 
+# Within the {Flight} model, a {Flight} contains both a reference to a Trip ID
+# and an integer trip section number. Trip sections are used to distinguish
+# between layovers and multiple visits to a given airport within a given Trip,
+# in the situation where two {Flight Flights} are chronologically consecutive
+# and the destination {Airport} of the first {Flight} is the same as the origin
+# of the second. If these two flights share the same Trip ID and trip section,
+# then the time between the two {Flight Flights} is a layover and only counts
+# as one visit to shared {Airport}. Otherwise, the traveler left the airport in
+# between the {Flight Flights}, and it counts as two separate visits to the
+# shared {Airport}.
+#
+# Trips can be marked as hidden, which means they (and their {Flight Flights})
+# will not be included in views and flight data for anybody other than the
+# {User} who they belong to.
 class Trip < ApplicationRecord
   has_many :flights, dependent: :destroy
   belongs_to :user
   
   validates :user_id, presence: true
-  
-  # Takes a flight departure time, and returns the trip section this flight
-  # most likely belongs to.
+  validates :name, :presence => true
+
+  # Form fields which should be saved as nil when the field is blank.
+  NULL_ATTRS = %w( comment )
+  before_save :nil_if_blank
+    
+  # Takes a flight departure time, and returns the trip section this {Flight}
+  # most likely belongs to. Used while prepopulating the {FlightsController#new
+  # new Flight} form to guess whether a new {Flight} is more likely to be part
+  # of the previous flight's trip section or a new trip section.
+  # 
+  # @param departure_utc [DateTime] the UTC departure time of the new {Flight}
+  # @return [Integer] the most likely trip section for this new flight
   def estimated_trip_section(departure_utc)
     flights = self.flights.chronological
     if flights.any?
@@ -20,6 +46,11 @@ class Trip < ApplicationRecord
     end
   end
   
+  # Returns a hash of possible trip purposes. Used to populate the trip
+  # purpose select box on the {TripsController#new new} or
+  # {TripsController#edit edit} Trip forms.
+  #
+  # @return [Hash] trip purposes
   def self.purposes_list
     purposes = Hash.new
     purposes["business"] = "Business"
@@ -28,7 +59,13 @@ class Trip < ApplicationRecord
     return purposes
   end
   
-  # Returns a collection of trips with departure dates (filtered by visitor if appropriate)
+  # Returns a collection of trip IDs, names, and departure dates. Used in
+  # {TripsController#index} to show a list of trips.
+  #
+  # @param flyer [User] the {User} whose trips are to be shown
+  # @param current_user [User, nil] the {User} (or visitor if nil) viewing the
+  #   list of trips
+  # @return [Array<Hash>] an array of trip details
   def self.with_departure_dates(flyer, current_user)
     if flyer == current_user
       trips = Trip.find_by_sql(["SELECT flights.trip_id, trips.id, trips.name, trips.hidden, MIN(flights.departure_date) AS departure_date FROM flights JOIN trips ON flights.trip_id = trips.id WHERE trips.user_id = 1 GROUP BY flights.trip_id, trips.id, trips.name, trips.hidden ORDER BY departure_date", flyer.id])
@@ -39,17 +76,20 @@ class Trip < ApplicationRecord
     return trips
   end
   
+  # Returns a collection of Trips which have no {Flight Flights}. Used in
+  # {TripsController#index} to allow a verified {User} to select a Trip which
+  # they have not yet added any {Flight Flights} to.
+  #
+  # @return [Array<Trip>] Trips with no {Flight Flights}
   def self.with_no_flights
     return Trip.where("id not in (?)",Trip.distinct.joins(:flights).select("trips.id"))
   end
-  
-  NULL_ATTRS = %w( comment )
-  before_save :nil_if_blank
-  
-  validates :name, :presence => true
-  
+    
   protected
-  
+
+  # Converts blank form fields to nil before saving them to the database.
+  #
+  # @return [Hash]
   def nil_if_blank
     NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
   end
