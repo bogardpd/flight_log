c645b35df07d6ebd0bb4c4e85df4717bc66e1c6e
Paul Bogard <paulbogard@gmail.com>
2019-06-03 21:12:24 -0400

    Document User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
+# Defines a model for users.
+# 
+# Since multi-user functionality is not yet implemented in this application,
+# this model is only used to support a single user and has very little
+# implemented functionality at this time. If multi-user functionality is added,
+# this model will require substantial modification.
 class User < ApplicationRecord
   has_secure_password
   has_many :trips, dependent: :destroy
@@ -9,13 +15,20 @@ class User < ApplicationRecord
   validates :password_confirmation, :presence => true
   validates :email, :presence => true
   
-  # Returns both emails associated with a user.
+  # Returns both email addresses associated with this User. Used by
+  # {ApplicationController#check_email_for_boarding_passes} to match received
+  # emails to a User.
+  # 
+  # @return [Array<String>] email addresses
   def all_emails
     return [self.email, self.alternate_email]
   end
   
-  # Returns all of this user's flights that viewing_user is allowed to see.
-  # If the viewer is a visitor, viewing_user should be nil.
+  # Returns all of this User's flights that the viewing user/visitor has
+  # permission to see.
+  #
+  # @param viewing_user [User] the {User} (or visitor if nil) viewing the flights
+  # @return [Array<Flight>] a collection of flights
   def flights(viewing_user=nil)
     if self == viewing_user
       # The viewer is viewing their own flights, so get all trips.
@@ -27,16 +40,22 @@ class User < ApplicationRecord
     return Flight.where(trip_id: trip_ids.sort).order(:departure_utc)
   end
     
-  # Returns the hash digest of the given string.
+  # Returns the hash digest of the given string. Used for hashing User passwords.
+  # 
+  # @param string [String] the password to hash
+  # @return [String] a hashed password
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
   
   private
-    
-    def create_remember_token
-      self.remember_token = SecureRandom.hex
-    end
+  
+  # Creates a token for storing in session cookies to remember a user.
+  # 
+  # @return [String] a token
+  def create_remember_token
+    self.remember_token = SecureRandom.hex
+  end
     
 end
