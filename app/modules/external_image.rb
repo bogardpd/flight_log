5608126d805fb766b2205e97349de2c684a9baae
Paul Bogard <paulbogard@gmail.com>
2019-06-05 21:51:48 -0400

    Document ExternalImage

--- app/modules/external_image.rb
+++ app/modules/external_image.rb
@@ -1,8 +1,17 @@
+# Provides utilities for dealing with images hosted on an image storage service.
 module ExternalImage
   
+  # The root URL for this application's external images.
   ROOT_PATH = "https://s3.us-east-2.amazonaws.com/pbogardcom-images"
   
-  # Returns true if the image at path exists, false otherwise.
+  # Checks if an image exists.
+  # 
+  # Note that this method checks an absolute path, so {ROOT_PATH} is not
+  # automatically prepended. Be sure to manually include {ROOT_PATH} in the path
+  # string if the image is hosted there.
+  #
+  # @param path [String] the absolute path to check
+  # @return [Boolean] true if the image at path exists, false otherwise
   def self.exists?(path)
     require "net/http"
     url = URI.parse(path)
