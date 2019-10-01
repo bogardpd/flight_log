a694f9e6394db2205a38df913413b5166ee281e4
Paul Bogard <paulbogard@gmail.com>
2019-09-30 22:03:09 -0400

    Explicitly set production environment to serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Portfolio::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
+  config.serve_static_assets = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
