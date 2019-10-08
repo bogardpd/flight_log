e09123679f8352ee16de5999258dc25dc51ff738
Paul Bogard <paulbogard@gmail.com>
2019-10-08 08:44:18 -0400

    Write integration test for LetsEncrypt response

--- test/integration/pages_flow_test.rb
+++ test/integration/pages_flow_test.rb
@@ -20,11 +20,25 @@ class PagesFlowTest < ActionDispatch::IntegrationTest
 
   test "can see active trips on home when logged in" do
     # At least one Trip must have hidden: true for this test to succeed.
+    # At least one PKPass must be present for this test to succeed.
     log_in_as users(:user_one)
     get "/"
     assert_response :success
     assert_select "div.message-info", /Active Trip/, "This view must list an active trip"
     assert_select "a:match('href', ?)", "/new-flight-menu", {}, "This view must show a link to import boarding passes"
+  end
+
+  test "can see letsencrypt" do
+    key = "C_EF567890"
+    cached_lets_encrypt_key = ENV["LETS_ENCRYPT_KEY"]
+    ENV["LETS_ENCRYPT_KEY"] = key
+
+    get "/.well-known/acme-challenge/ABCD1234"
+    assert_response :success
+    assert_equal "text/plain", response.content_type
+    assert_equal key, response.body
+
+    ENV["LETS_ENCRYPT_KEY"] = cached_lets_encrypt_key
   end
 
 end
