3006e82ad94b6fa98b6965e82ff670e85eeb81da
Paul Bogard <paulbogard@gmail.com>
2019-10-18 13:58:55 -0400

    Write integration tests for Index Airlines view

--- test/integration/airline_flows_test.rb
+++ test/integration/airline_flows_test.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 
 class AirlineFlowsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @visible_airline = airlines(:airline_visible)
+    @hidden_airline = airlines(:airline_hidden)
+    @visible_operator = airlines(:operator_visible)
+    @hidden_operator = airlines(:operator_hidden)
+    @no_flights_airline = airlines(:airline_no_flights)
+  end
   
   ##############################################################################
   # Tests for Spec > Pages (Views) > Add/Edit Airline                          #
@@ -65,11 +73,62 @@ class AirlineFlowsTest < ActionDispatch::IntegrationTest
     log_in_as(users(:user_one))
     get(airlines_path)
     assert_response(:success)
+
+    assert_select("h1", "Airlines")
+    assert_select("table#airline-count-table") do
+      check_airline_flight_row(@visible_airline, "This view should show airlines with visible flights")
+      check_airline_flight_row(@hidden_airline, "This view should show airlines with only hidden flights when logged in")
+    end
+    assert_select("table#operator-count-table") do
+      check_operator_flight_row(@visible_operator, "This view should show operators with visible flights")
+      check_operator_flight_row(@hidden_operator, "This view should show operators with only hidden flights when logged in")
+    end
+    assert_select("table#airlines-with-no-flights-table") do
+      assert_select("tr#airline-with-no-flights-row-#{@no_flights_airline.id}", {}, "This view should show airlines with no flights when logged in") do
+        assert_select("a[href=?]", airline_path(id: @no_flights_airline.slug))
+      end
+    end
+
+    assert_select("div#admin-actions", {}, "This view should show admin actions when logged in") do
+      assert_select("a[href=?]", new_airline_path, {}, "This view should show a New Airline link when logged in")
+    end
+
   end
 
   test "can see index airlines when not logged in" do
     get(airlines_path)
     assert_response(:success)
+
+    assert_select("h1", "Airlines")
+    assert_select("table#airline-count-table") do
+      check_airline_flight_row(@visible_airline, "This view should show airlines with visible flights")
+      assert_select("tr#airline-count-row-#{@hidden_airline.id}", {count: 0}, "This view should not show airlines with only hidden flights when not logged in")
+    end
+    assert_select("table#operator-count-table") do
+      check_operator_flight_row(@visible_operator, "This view should show operators with visible flights")
+      assert_select("tr#operator-count-row-#{@hidden_operator.id}", {count: 0}, "This view should not show operators with only hidden flights when not logged in")
+    end
+    assert_select("table#airlines-with-no-flights-table", {count: 0}, "This view should not show airlines with no flights when not logged in")
+
+    assert_select("div#admin-actions", {count: 0}, "This view should not show admin actions when not logged in")
+    assert_select("a[href=?]", new_airline_path, {count: 0}, "This view should not show a New Airline link when not logged in")
+
+  end
+
+  private
+
+  def check_airline_flight_row(airline, error_message)
+    assert_select("tr#airline-count-row-#{airline.id}", {}, error_message) do
+      assert_select("a[href=?]", airline_path(id: airline.slug))
+      assert_select("text.graph-value", airline.flights.count.to_s)
+    end
+  end
+
+  def check_operator_flight_row(operator, error_message)
+    assert_select("tr#operator-count-row-#{operator.id}", {}, error_message) do
+      assert_select("a[href=?]", show_operator_path(operator: operator.slug))
+      assert_select("text.graph-value", operator.operated_flights.count.to_s)
+    end
   end
 
 end
