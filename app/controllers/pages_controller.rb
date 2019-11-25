a5896f8bd1db0b5bed0da26253f254bd2eb9069f
Paul Bogard <paulbogard@gmail.com>
2019-11-24 21:54:29 -0500

    Increase cache time on gcmap_image_proxy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -72,7 +72,7 @@ class PagesController < ApplicationController
     query = params[:query].gsub("_","/")
     
     if Map.hash_image_query(query) == params[:check] # Ensure the query was issued by this application
-      response.headers["Cache-Control"] = "public, max-age=#{84.hours.to_i}"
+      response.headers["Cache-Control"] = "public, max-age=#{1.year.to_i}"
       response.headers["Content-Type"] = "image/gif"
       response.headers["Content-Disposition"] = "inline"
       image_url = "http://www.gcmap.com/map?PM=#{params[:airport_options]}&MP=r&MS=wls2&P=#{query}"
