a12cf2672ab918ee1ec92f7b8beeec27698dd792
Paul Bogard <paulbogard@gmail.com>
2020-02-23 12:23:10 -0500

    Create RouteHelper.format_route_with_abbr method

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -5,9 +5,19 @@ module RoutesHelper
   # codes separated by two-way arrows.
   # 
   # @param codes [Array<String>] an array of airport codes
-  # @return [ActiveSupport::SafeBuffer] a formatted route string
+  # @return [Object] a formatted route string
   def format_route(codes)
     return safe_join(codes.map{|airport| content_tag(:span, airport, class: "code-mono")}, " #{Route::ARROW_TWO_WAY_PLAINTEXT} ")
+  end
+
+  # Converts an array of airport codes and an array of airport cities into a
+  # string of airport code abbr tags with city titles.
+  #
+  # @param codes [Array<String>] an array of airport codes
+  # @param cities [Array<String>] an array of city names
+  # @return [Object] an HTML-formatted route string
+  def format_route_with_abbr(codes, cities)
+    return safe_join([content_tag(:abbr, codes[0], title: cities[0]), Route::ARROW_TWO_WAY_PLAINTEXT, content_tag(:abbr, codes[1], title: cities[1])], " ")
   end
   
 end
