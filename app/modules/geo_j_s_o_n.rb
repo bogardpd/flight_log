ffd5fe3cb71046550f409c9345f4b2163ddcb93d
Paul Bogard <paulbogard@gmail.com>
2023-02-09 23:05:50 -0500

    Write GeoJSON.flights_to_geojson

--- app/modules/geo_j_s_o_n.rb
+++ app/modules/geo_j_s_o_n.rb
@@ -2,6 +2,8 @@
 module GeoJSON
   
   TEMP_FILE = "tmp/flights.geojson"
+  COORD_DIGITS = 3 # Number of digits to round coordinates to
+  DEG_INTERVAL = 1.0 # Degrees per step for great circle route points
 
   # Generate GeoJSON output from a collection of {Flight flights}.
   #
@@ -13,20 +15,40 @@ module GeoJSON
   # 
   # @see https://geojson.org/
   def self.flights_to_geojson(flights)
-    flights = flights.includes(:origin_airport, :destination_airport, :airline)
-
-    airport_points = Airport.visit_table_data(flights).map{|a| {id: a[:id], iata: a[:iata_code], visits: a[:visit_count]}}
-    flight_line_strings = Hash.new()
+    flights = flights.includes(:origin_airport, :destination_airport)
+    
+    airport_visits = Airport.visit_table_data(flights).to_h{|a| [a[:id], a[:visit_count]]}
+    airport_data = Airport.where(id: airport_visits.keys)
+      .pluck(:id, :iata_code, :latitude, :longitude)
+      .to_h{|a| [a[0], {
+        iata_code: a[1],
+        latitude: a[2],
+        longitude: a[3]}]}
+    airport_features = airport_data.map{|id, airport| {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          airport[:longitude].round(COORD_DIGITS),
+          airport[:latitude].round(COORD_DIGITS)]
+      },
+      properties: {
+        iata: airport[:iata_code],
+        freq: airport_visits[id],
+      }
+    }}
+    
+    route_data = Hash.new()
     flights.each do |flight|
       route_id = [flight.origin_airport_id, flight.destination_airport_id].sort
       for_rev = flight.origin_airport_id < flight.destination_airport_id ? [1,0] : [0,1]
       freqs = {freq: 1, freq_forward: for_rev[0], freq_reverse: for_rev[1]}
-      if flight_line_strings.key?(route_id)
+      if route_data.key?(route_id)
         # Route exists in hash. Add frequencies.
-        freqs.each{|k,v| flight_line_strings[route_id][k] += v}
+        freqs.each{|k,v| route_data[route_id][k] += v}
       else
         # Add route to hash.
-        flight_line_strings[route_id] = {
+        route_data[route_id] = {
           orig_iata: flight.origin_airport.iata_code,
           orig_coord: Coordinate.new(*flight.origin_airport.coordinates),
           dest_iata: flight.destination_airport.iata_code,
@@ -35,12 +57,50 @@ module GeoJSON
         }
       end
     end
+    route_features = route_data.map{|id, f| {
+      type: "Feature",
+      geometry: {
+        type: "MultiLineString",
+        coordinates: GreatCircle
+          .gc_route_coords(f[:orig_coord], f[:dest_coord], DEG_INTERVAL)
+          .map{|line_string| line_string
+            .map{|point| [
+              point.lon.round(COORD_DIGITS),
+              point.lat.round(COORD_DIGITS),
+            ]}
+          }
+      },
+      properties: {
+        orig_iata: f[:orig_iata],
+        dest_iata: f[:dest_iata],
+        freq: f[:freq],
+        freq_forward: f[:freq_forward],
+        freq_reverse: f[:freq_reverse],
+      }
+    }}    
     
-    # TODO: convert airport_points and flight_line_strings to GeoJSON.
-    pp flight_line_strings
+    output = {
+      type: "FeatureCollection",
+      features: [*airport_features, *route_features]
+    }
 
-    # write_temp_file(output)
+    gj_output = output.to_json
+    write_temp_file(gj_output)
+    return gj_output
 
+  end
+
+  private
+
+  # Writes JSON to a temporary file.
+  # 
+  # @param json [Object] JSON to write to file
+  # @return [nil]
+  def self.write_temp_file(json)
+    f = File.open(TEMP_FILE, "w")
+    f << json
+    f.close
+    return nil
   end
 
 end
