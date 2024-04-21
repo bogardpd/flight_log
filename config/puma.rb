f25ae0e589feb10d12fec3a81b7c8342a52bbb2f
Paul Bogard <paulbogard@gmail.com>
2024-04-21 12:22:46 -0400

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,15 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
-rackup      DefaultRackup
+rackup      DefaultRackup if defined?(DefaultRackup)
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
+  # Worker-specific setup for Rails 4.1 to 5.2, after 5.2 it's not needed
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
